ebc75bc8d7bac4260aa06d447ce6a9725c3d2cca
Emily Elisabeth Neves <95760996+EmilyEN341@users.noreply.github.com>
2022-08-22 17:47:01 -0300

    Add files via upload

---
+++ ex3.java
@@ -0,0 +1,15 @@
+public class ex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	
+	public Double calculaAbastecimento(Double qtdLitros, Double precoLitro) {
+		if (qtdLitros == null) {
+			
+		}
+		return qtdLitros * precoLitro;
+	}
+}
+
